9f65129769cfae7ee1eec09142a67a0b64c20e9a
jinjae84 <jinjae84@naver.com>
2024-11-12 21:44:07 +0900

    (2024.11.12) 단일 연결 리스트

---
+++ Program/Program.cpp
@@ -0,0 +1,60 @@
+﻿#include <iostream>
+
+using namespace std;
+
+template<typename T>
+class SingleLinkedList
+{	
+private:
+	struct  Node
+	{
+		T data;
+		Node* next;
+	};
+	
+	int size;
+	
+	Node* head;
+
+public:
+	SingleLinkedList()
+	{
+		size = 0;
+		head = nullptr;
+	}
+	
+	void push_front(T data)
+	{
+
+
+		if (head == nullptr)
+		{
+			head = new Node;
+
+			head->data = data;
+			head->next = nullptr;
+		}
+		else
+		{
+			Node * newNode = new Node;
+
+			newNode->data = data;
+			newNode->next = head;
+
+			head = newNode;
+		}
+
+		size++;
+	}
+};
+
+
+
+int main()
+{
+	SingleLinkedList<int> singleLinkedList;
+
+	singleLinkedList.push_front(10);
+	singleLinkedList.push_front(20);  //  [20] <------ [10]
+    
+}
