f43eaf810336c71b3ffcd135354fb870bdcddc17
jinjae84 <jinjae84@naver.com>
2024-11-20 21:44:22 +0900

    (2024.11.20) 벡터

--- Program/Program.cpp
+++ Program/Program.cpp
@@ -2,32 +2,76 @@
 
 using namespace std;
 
-#define SIZE 10
+#define SIZE 5
 
 template<typename T>
-class Stack
+class LinearQueue
 {
 private:
-	
-	int top;
-	
+	int front;
+	int rear;
+	int size;
 	T container[SIZE];
-
 public:
-	Stack()
+	LinearQueue()
 	{
-		top = -1;
-
+		front = 0;
+	    rear = 0;
+		size = 0;
 		for (int i = 0; i < SIZE; i++)
 		{
 			container[i] = NULL;
 		}
 	}
+	void Push(T data)
+	{
+		if (rear >= SIZE)
+		{
+			cout << "Linear Queue Overflow" << endl;
+		}
+		else
+		{
+			container[rear++] = data;
+
+			size++;
+		}
+	}
+	
+	void Pop()
+	{
+		if (Empty())
+		{
+			cout << "Linear Queue is Empty" << endl;
+		}
+		else
+		{
+			front++;
+			size--;
+		}
+
+	}
+
+	bool Empty
+	{
+		if (rear <= 0)
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}		
+	}
 };
 
 int main()
 {
-	Stack<int> stack;
-
+	LinearQueue<int> linearQueue;
+	
+	linearQueue.Push(10);
+	linearQueue.Push(20);
+	linearQueue.Pop();
+	linearQueue.Pop();
+	linearQueue.Pop();
 	return 0;
 }
