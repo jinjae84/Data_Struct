18f559bfe259ee0cabd7ceaea75f4b944f9a4e3f
jinjae84 <jinjae84@naver.com>
2024-11-25 21:42:03 +0900

    (2024.11.25) 해시 테이블

--- Program/Program.cpp
+++ Program/Program.cpp
@@ -2,85 +2,53 @@
 
 using namespace std;
 
-template<typename T>
-class Vector
+class String
 {
 private:
-	T* container;
-	int size;
-	int capacity;
+	int size;	
+	char* container;
 public:
-	Vector()
+	String()
 	{
-		size = 0;
-		capacity = 0;
+		size = 0;		
 		container = nullptr;
 	}
 
-	~Vector()
+	~String()
 	{
 		if (container != nullptr)
 		{
-			delete[] container;
+			delete container;
 		}
 	}
 
-	void Resize(int newSize)
+	void operator = (const char* word)
 	{
-		// 1. capacity에 새로운 size값을 저장합니다.
-		capacity = newSize;
+		int arraySize = strlen(word) +1;
 
-		// 2. 새로운 포인터 변수를 생성해서 새롭게 만들어진
-		//    메모리 공간을 가리키도록 합니다.
-		T* newContainer = new T[capacity];
+		size = strlen(word);
 
-		// 3. 새로운 메모리 공간의 값을 초기화합니다.
-		for (int i = 0; i < capacity; i++)
+		if (container == nullptr)
 		{
-			newContainer[i] = NULL;
-		}
+			container = new char[size + 1];
 
-		// 4. 기존 배열에 있는 값을 복사해서 새로운 배열
-		//    에 넣어줍니다.
-		for (int i = 0; i < size; i++)
-		{
-			newContainer[i] = container[i];
-		}
-
-		// 5. 기존 배열의 메모리를 해제합니다.
-		if (container != nullptr)
-		{
-			delete[] container;
-		}
-
-		// 6. 기존에 배열을 가리키면 포인터 변수의 값을
-		//    새로운 배열의 시작 주소로 가리킵니다.
-		container = newContainer;
-	}
-
-	void PushBack(T data)
-	{		
-		if (capacity <= 0)
-		{
-			Resize(1);
-		}
-		else if (size >= capacity)
-		{
-			Resize(capacity * 2);
-		}
-
-		container[size++] = data;
-	}
-
-	void PopBack()
-	{
-		if (size <= 0)
-		{
-			cout << "Vector is Empty" << endl;						
+			for (int i = 0; i < arraySize; i++)
+			{
+				container[i] = word[i];
+			}
 		}
 		else
 		{
-			container[--size] = NULL;
+			char* newContainer = new char[arraySize];
+
+			for (int i = 0; i < arraySize; i++)
+			{
+				newContainer[i] = word[i];
+			}
+
+			delete[] container;
+
+			container = newContainer;
 		}
 	}
 
@@ -89,28 +57,29 @@ public:
 		return size;
 	}
 
-	T& operator[](int index)
+	char& operator[](const int& index)
 	{
 		return container[index];
-	}
-
-	void Reserve(int newSize)
-	{
-		
 	}
 };
 
 int main()
 {
-	Vector<int> vector;
-
-	vector.PushBack(10);
-	vector.PushBack(20);
-	vector.PushBack(30);
-	vector.PushBack(40);
-	vector.PushBack(50);
-		
-	vector.PopBack();
+	String string;
 	
+	string = "Janna";
+	
+	for (int i = 0; i < string.Size(); i++)
+	{
+		cout << string[i] << "";
+	}
+	cout << endl;
+
+	string = "Bard";
+	for (int i = 0; i < string.Size(); i++)
+	{
+		cout << string[i] << "";
+	}
+	cout << endl;
 	return 0;
 }
