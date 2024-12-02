d554f2a4727d54706b9f188e51285da94d57eba3
jinjae84 <jinjae84@naver.com>
2024-12-02 19:19:53 +0900

    (2024.12.01)

---
+++ Class 9th (Binary Tree)/Source.cpp
@@ -0,0 +1,100 @@
+﻿#include <iostream>
+
+using namespace std;
+
+struct Node
+{
+	int data;
+
+	Node* left;
+
+	Node* right;
+};
+
+Node* CreateNode(int data, Node* left, Node* right)
+{
+	Node* newNode = new Node();
+
+	newNode->data = data;
+
+	newNode->left = left;
+
+	newNode->right = right;
+
+	return newNode;
+}
+
+void PreOrder(Node* root)
+{
+	if (root == nullptr)
+	{
+		return;
+	}
+	else
+	{
+		cout << root->data << " ";
+
+		PreOrder(root->left);
+
+		PreOrder(root->right);
+	}
+}
+
+void InOrder(Node* root)
+{
+	if (root == nullptr)
+	{
+		return;
+	}
+	else
+	{
+		InOrder(root->left);
+
+		cout << root->data << " ";
+
+		InOrder(root->right);
+	}
+}
+
+void PostOrder(Node* root)
+{
+	if (root == nullptr)
+	{
+		return;
+	}
+	else
+	{
+		PostOrder(root->left);
+
+		PostOrder(root->right);
+
+		cout << root->data << " ";
+	}
+}
+
+int main()
+{
+	Node* node5 = CreateNode(5, nullptr, nullptr);
+	Node* node4 = CreateNode(4, nullptr, nullptr);
+	Node* node3 = CreateNode(3, nullptr, nullptr);
+	Node* node2 = CreateNode(2, node4, node5);
+	Node* node1 = CreateNode(1, node2, node3);
+
+	// 전위 순회
+	// PreOrder(node1); 	
+
+	// 중위 순회
+	// InOrder(node1);
+
+	// 후위 순회
+	// PostOrder(node1);
+
+	delete node5;
+	delete node4;
+	delete node3;
+	delete node2;
+	delete node1;
+
+	return 0;
+}
+
--- Program/Program.cpp
+++ Program/Program.cpp
@@ -2,98 +2,169 @@
 
 using namespace std;
 
-struct Node
+#define SIZE 6
+
+template<typename KEY, typename VALUE>
+class HashTable
 {
-	int data;	
-	
-	Node * left;
-	
-	Node * right;		
+private:
+	struct Node
+	{
+		KEY key;
+		VALUE value;
+
+		Node* next;
+	};
+
+	struct Bucket
+	{
+		int count;
+		Node* head;
+	};
+
+	Bucket bucket[SIZE];
+
+public:
+	HashTable()
+	{
+		for (int i = 0; i < SIZE; i++)
+		{
+			bucket[i].count = 0;
+			bucket[i].head = nullptr;
+		}
+	}
+
+	template<typename T>
+	int HashFunction(T key)
+	{
+		int hashIndex = (int)key % SIZE;
+
+		return hashIndex;
+	}
+
+	template<>
+	int HashFunction(std::string key)
+	{
+		int result = 0;
+
+		for (int i = 0; i < key.length(); i++)
+		{
+			result += key[i];
+		}
+
+		int hashIndex = result % SIZE;
+
+		return hashIndex;
+	}
+
+	void Insert(KEY key, VALUE value)
+	{
+		// 해시 함수를 통해서 값을 받는 임시 변수
+		int hashIndex = HashFunction(key);
+
+		// 새로운 노드를 생성합니다.
+		Node* newNode = CreateNode(key, value);
+
+		// 노드가 1개라도 존재하지 않는다면
+		if (bucket[hashIndex].count == 0)
+		{
+			// bucket[hashIndex]의 head 포인터를 newNode를 가리키게 합니다.
+			bucket[hashIndex].head = newNode;
+		}
+		else
+		{
+			newNode->next = bucket[hashIndex].head;
+
+			bucket[hashIndex].head = newNode;
+		}
+		// bucket[hashIndex]의 count를 증가합니다.
+		bucket[hashIndex].count++;
+	}
+
+	void Remove(KEY key)
+	{
+		// 1. 해시 함수를 통해서 값을 받는 임시 변수
+		int hashIndex = HashFunction(key);
+
+		// 2. Node를 탐색할 수 있는 포인터 변수 선언
+		Node* currentNode = bucket[hashIndex].head;
+
+		// 3. 이전 Node를 저장할 수 있는 포인터 변수 선언
+		Node* previousNode = nullptr;
+
+		// 4. currentNode가 nullptr과 같다면 함수를 종료합니다.
+		if (currentNode == nullptr)
+		{
+			cout << "Not Key Found" << endl;
+
+			return;
+		}
+	}
+
+	void Show()
+	{
+		for (int i = 0; i < SIZE; i++)
+		{
+			Node* currentNode = bucket[i].head;
+
+			while (currentNode != nullptr)
+			{
+				cout << "[" << i << "]" << "KEY : " << currentNode->key << "VALUE : " << currentNode->value << " ";
+				currentNode = currentNode->next;
+			}
+
+			cout << endl;
+		}
+	}
+
+	Node* CreateNode(KEY key, VALUE value)
+	{
+		Node* newNode = new Node();
+
+		newNode->key = key;
+
+		newNode->value = value;
+
+		newNode->next = nullptr;
+
+		return newNode;
+	}
+
+	~HashTable()
+	{
+		for (int i = 0; i < SIZE; i++)
+		{
+			Node* deleteNode = bucket[i].head;
+			Node* nextNode = bucket[i].head;
+
+			if (bucket[i].head == nullptr)
+			{
+				continue;
+			}
+			else
+			{
+				while (nextNode != nullptr)
+				{
+					nextNode = deleteNode->next;
+
+					delete deleteNode;
+
+					deleteNode = nextNode;
+				}
+			}
+		}
+	}
 };
-
-Node * CreateNode(int data, Node* left, Node* right)
-{
-	Node * newNode = new Node();
-
-	newNode->data = data;
-
-	newNode->left = left;
-	
-	newNode->right = right;
-	
-	return newNode;
-}
-
-void PreOrder(Node* root)
-{
-	if (root == nullptr)
-	{
-		return;
-	}
-	else
-	{		
-		cout << root->data << " ";
-		
-		PreOrder(root->left);
-		
-		PreOrder(root->right);
-	}
-}
-
-void InOrder(Node* root)
-{
-	if (root == nullptr)
-	{
-		return;
-	}
-	else
-	{
-		InOrder(root->left);
-
-		cout << root->data << " ";
-				
-		InOrder(root->right);
-	}
-}
-
-void PostOrder(Node* root)
-{
-	if (root == nullptr)
-	{
-		return;
-	}
-	else
-	{
-		PostOrder(root->left);
-
-		PostOrder(root->right);
-
-		cout << root->data << " ";	
-	}
-}
-
 int main()
 {
-	Node* node5 = CreateNode(5, nullptr, nullptr);
-	Node* node4 = CreateNode(4, nullptr, nullptr);
-	Node* node3 = CreateNode(3, nullptr, nullptr);
-	Node * node2 = CreateNode(2, node4, node5);	
-	Node * node1 = CreateNode(1, node2, node3);
-	
-	// 전위 순회
-	// PreOrder(node1); 	
+	HashTable<int, std::string> hashTable;
 
-	// 중위 순회
-	// InOrder(node1);
-	
-	// 후위 순회
-	// PostOrder(node1);
+	hashTable.Insert(10, "Vector");
+	hashTable.Insert(15, "Stack");
+	hashTable.Insert(6, "Queue");
+	hashTable.Insert(20, "Linked List");
 
-	delete node5;
-	delete node4;
-	delete node3;
-	delete node2;
-	delete node1;
+	hashTable.Show();
 
 	return 0;
 }
