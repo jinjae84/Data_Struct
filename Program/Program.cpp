dfc047905fccb87657a26ec3cc31d048afcf92aa
jinjae84 <jinjae84@naver.com>
2024-12-10 21:44:41 +0900

    (2024.12.10) 트리의 순회

--- Program/Program.cpp
+++ Program/Program.cpp
@@ -23,7 +23,19 @@ public:
 
 	~BinarySearchTree()
 	{
-		
+		Destroy(root);		
+	}
+
+	void Destroy(Node* root)
+	{
+		if (root != nullptr)
+		{
+			Destroy(root->left);
+
+			Destroy(root->right);
+
+			delete root;
+		}		
 	}
 
 	Node * RootNode()
@@ -90,18 +102,123 @@ public:
 
 	void Inorder(Node * root)
 	{
-		if (root == nullptr)
-		{
-			return;
-		}
-		else
+		if (root != nullptr)
 		{
 			Inorder(root->left);
 
 			cout << root->data << " ";
 
 			Inorder(root->right);
+		}		
+	}
+
+	bool Find(T data)
+	{
+		Node* currentNode = root;
+
+		if (currentNode == nullptr)
+		{
+			return false;
 		}
+		else
+		{
+			while (currentNode != nullptr)
+			{
+				if (currentNode->data == data) {
+					return true;
+				}
+				else
+				{
+					if (currentNode->data > data)
+					{
+						currentNode = currentNode->left;
+					}
+					else
+					{
+						currentNode = currentNode->right;
+					}
+				}				
+			}
+
+			return false;
+		}		
+	}
+
+	void Remove(T data)
+	{
+		if (root == nullptr)
+		{
+			cout << "BinarySearchTree is Empty" << endl;
+		}
+		else
+		{
+			Node* currentNode = root;
+
+			Node* parentNode = nullptr;
+
+			while (currentNode != nullptr && currentNode->data != data)
+			{
+				if (currentNode->data > data)
+				{
+					parentNode = currentNode;
+
+					currentNode = currentNode->left;
+				}
+				else
+				{
+					parentNode = currentNode;
+
+					currentNode = currentNode->right;
+				}
+			}
+
+			if (currentNode == nullptr)
+			{
+				cout << "Data Not Found in the Binary Search Tree" << endl;
+			}
+
+			else if (currentNode->left == nullptr && currentNode->right == nullptr)
+			{
+				if (parentNode != nullptr)
+				{
+					if (parentNode->left == currentNode)
+					{
+						parentNode->left = nullptr;
+					}
+					else
+					{
+						parentNode->right = nullptr;
+					}
+				}
+				else
+				{
+					root = nullptr;
+				}
+			}
+
+			if (currentNode->left == nullptr || currentNode->right == nullptr)
+			{
+				Node* child;
+
+				if (parentNode != root)
+				{
+					if (parentNode->left == currentNode)
+					{
+						parentNode->left = child;
+					}
+					else
+					{
+						parentNode->right = child;
+					}
+				}
+				else
+				{
+					root = child;
+				}
+			}
+		}
+
+		
 	}
 };
 
@@ -109,12 +226,14 @@ int main()
 {
 	BinarySearchTree<int> binarySearchTree;
 
+	binarySearchTree.Insert(10);
 	binarySearchTree.Insert(15);
 	binarySearchTree.Insert(7);
-	binarySearchTree.Insert(9);
-	binarySearchTree.Insert(20);
+	binarySearchTree.Insert(8);
 
+	cout << binarySearchTree.Find(7) << endl;
+
+	binarySearchTree.Inorder(binarySearchTree.RootNode());
 	
-
 	return 0;
 }
